31069c75412271a31a3465055859bd3e6ba562d9
Arthur Almeida <arthur.almeidapereira@gmail.com>
2013-08-05 10:21:46 -0300

    removendo números múltiplos do primeiro elemento da lista

--- src/CrivoErastotenes.php
+++ src/CrivoErastotenes.php
@@ -2,10 +2,35 @@
 
 class CrivoErastotenes
 {
+	private $limit;
+
+	public function __construct($limit)
+	{
+		$this->limit = $limit;
+	}
 
 	public function numerosPrimos()
 	{
-		return array(2);
+		$listaCompleta 	= range(2, $this->limit);
+		$novaLista		= array();
+
+		if ($this->limit == 2) {
+			return array(2);
+		} elseif ($this->limit == 3) {
+			return array(2,3);
+		}
+
+		$novaLista[] = 2;
+
+		foreach ($listaCompleta as $numero) {
+
+			if ($numero % $listaCompleta[0] != 0 
+					&& $numero != 2) {
+				$novaLista[] = $numero;
+			}
+		}
+
+		return $novaLista;
 	}
 
 }
--- tests/CrivoErastotenesTest.php
+++ tests/CrivoErastotenesTest.php
@@ -8,8 +8,19 @@ class CrivoErastotenesTest extends \PHPUnit_Framework_TestCase
 	public function testParaLimiteDoisDeveRetornarSomenteDois()
 	{
 		$crivo = new CrivoErastotenes(2);
-
 		$this->assertEquals(array(2), $crivo->numerosPrimos());
+	}
+
+	public function testParaLimiteTresDeveRetornarDoisETres()
+	{
+		$crivo = new CrivoErastotenes(3);
+		$this->assertEquals(array(2, 3), $crivo->numerosPrimos());	
+	}
+
+	public function testeDeveRemoverMultiplosDoPrimeiroNumeroPrimo()
+	{
+		$crivo = new CrivoErastotenes(10);
+		$this->assertEquals(array(2, 3, 5, 7, 9), $crivo->numerosPrimos());	
 	}
 
 }
